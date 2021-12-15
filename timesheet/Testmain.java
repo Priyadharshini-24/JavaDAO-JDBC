c622f599697551e3b6d1118056efc05f40e88204
Priyadharshini-24 <78081103+Priyadharshini-24@users.noreply.github.com>
2021-12-15 18:12:23 +0530

    Update Testmain.java

--- timesheet/Testmain.java
+++ timesheet/Testmain.java
@@ -9,28 +9,78 @@ public class Testmain {
 		System.out.println("Welcome to Timesheet App");
 		System.out.println("\n1.Register\n2.Login\nEnter ur choice");
 		int choice=Integer.parseInt(sc.nextLine());
-		switch(choice)
-		{
+		UserDAO userdao =null;
+		switch (choice) {
 		case 1:
-			UserDAO userdao=new UserDAO();
-			System.out.println("Enter first name:");
-			String firstname=sc.nextLine();
-			System.out.println("Enter last name:");
-			String lastname=sc.nextLine();
-			System.out.println("Enter User Name:");
-			String username=sc.nextLine();
-			System.out.println("Enter Password:");
-			String password=sc.nextLine();
-			if(username.matches("[a-z0-9.]+[@][a-z]+[.][]a-z]+") && password.matches("[A-Za-z0-9@#]{8,10}"))
-			{
-			System.out.println("valid username:");
-			User user=new User(firstname,lastname,username,password);
+			String firstname = null;
+			String lastname = null;
+			String username = null;
+			String password = null;
+			String confirmpassword = null;
+			userdao = new UserDAO();
+			int flag = 0;
+			do {
+				System.out.println("Enter first name:");
+				firstname = sc.nextLine();
+				if (firstname.matches("[A-Za-z]{3,}")) {
+					flag = 0;
+					break;
+				} else {
+					System.out.println("invalid first name");
+					flag = 1;
+				}
+			} while (flag == 1);
+
+			do {
+				System.out.println("Enter last name:");
+				lastname = sc.nextLine();
+				if (lastname.matches("[A-Za-z]{3,}")) {
+					flag = 0;
+					break;
+				} else {
+					System.out.println("invalid last name");
+					flag = 1;
+				}
+			} while (flag == 1);
+
+			do {
+				System.out.println("Enter User Name:");
+				username = sc.nextLine();
+				if (username.matches("[a-z0-9.]+[@][a-z]+[.][]a-z]+")) {
+					flag = 0;
+					break;
+				} else {
+					System.out.println("invalid username");
+					flag = 1;
+				}
+			} while (flag == 1);
+
+			do {
+				System.out.println("Enter Password:");
+				password = sc.nextLine();
+				if (password.matches("[A-Z]+[a-z]+[0-9]+[@#]+{8,10}")) {
+					flag = 0;
+					break;
+				} else {
+					System.out.println("invalid password");
+					flag = 1;
+				}
+			} while (flag == 1);
+
+			do {
+				System.out.println("Enter Confirm Password:");
+				confirmpassword = sc.nextLine();
+				if (password.equals(confirmpassword)) {
+					flag = 0;
+					break;
+				} else {
+					System.out.println("pls enter confirm password same as password");
+					flag = 1;
+				}
+			} while (flag == 1);
+
+			User user = new User(firstname, lastname, username, password);
 			userdao.insertuser(user);
-			}
-			else
-			{
-				System.out.println("invalid username or password");
-			}
 		case 2:
 			userdao=new UserDAO();
 			System.out.println("User Login");
